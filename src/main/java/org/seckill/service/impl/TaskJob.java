881865f7de1b48bdc80de49b176f21a4615c7a53
WaneLiu <825060952@qq.com>
2016-08-02 16:35:20 +0800

    add timertask

---
+++ src/main/java/org/seckill/service/impl/TaskJob.java
@@ -0,0 +1,13 @@
+package org.seckill.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by liufeng10 on 2016/8/2.
+ */
+@Service
+public class TaskJob {
+    public void job1() {
+        System.out.println("after task");
+    }
+}
